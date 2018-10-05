ab9ddcb11eec78a4847bbd4256501047b9fedfb4
Daniel J. B. Clarke <u8sand@gmail.com>
2018-10-05 15:40:34 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name='pyswaggerclient',
+  version='1.0',
+  packages=['pyswaggerclient'],
+  license='Apache-2.0',
+  long_description=open('README.md', 'r').read(),
+)
